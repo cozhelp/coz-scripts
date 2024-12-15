966ff78e28f1b59a2bff603e378d032426b81f24
Jim Cosner <cozhelp@gmail.com>
2024-12-14 22:48:02 -0600

    Changed how the del function works

--- windows/maintenance/cleanup-shortcuts.bat
+++ windows/maintenance/cleanup-shortcuts.bat
@@ -30,6 +30,7 @@ GoTo :EOF
 :Start
 REM Grab the path for the desktop from the registry
 for /f "tokens=2*" %%a in ('REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "Desktop"') do set "Desktop=%%~b"
+echo Desktop: %Desktop%
 Set Desktop=%UserProfile%\Desktop
 If Exist "%OneDrive%\Desktop" Set Desktop=%OneDrive%\Desktop
 GoTo :EOF
