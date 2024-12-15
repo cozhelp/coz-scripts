e94bd00b73bc23b0aca1e1a8645b0c356a37fc66
Jim Cosner <cozhelp@gmail.com>
2024-12-14 22:44:02 -0600

    Changed how the del function works

--- windows/maintenance/cleanup-shortcuts.bat
+++ windows/maintenance/cleanup-shortcuts.bat
@@ -1,33 +1,32 @@
 @Echo Off
 Call :Start
 
-Call :Del "C:\Users\Public\Desktop\Cisco Webex Meetings.lnk"
-Call :Del "C:\Users\Public\Desktop\Firefox.lnk"
-Call :Del "C:\Users\Public\Desktop\Oracle VM VirtualBox.lnk"
-Call :Del "C:\Users\Public\Desktop\Zoom.lnk"
-Call :Del "C:\Users\Public\Desktop\Zoom Workplace.lnk"
-Call :Del "C:\Users\Public\Desktop\Visual Studio Code.lnk"
-Call :Del "C:\Users\Public\Desktop\Nextcloud.lnk"
-Call :Del "C:\Users\Public\Desktop\Google Chrome.lnk"
-Call :Del "C:\Users\Public\Desktop\LibreOffice *.lnk"
-Call :Del "C:\Users\Public\Desktop\RingCentral.lnk"
-Call :Del "C:\Users\Public\Desktop\Dell Display Manager*.lnk"
-Call :Del "C:\Users\Public\Desktop\pCloud Drive.lnk"
-Call :Del "C:\Users\Public\Desktop\Mumble.lnk"
-Call :Del "C:\Users\Public\Desktop\Slack.lnk"
-Call :Del "%Desktop%\Discord.lnk"
-Call :Del "%Desktop%\RingCentral.lnk"
-Call :Del "%Desktop%\Vonage Business.lnk"
-Call :Del "%Desktop%\Brave.lnk"
-Call :Del "%Desktop%\Spotify.lnk"
-Call :Del "%Desktop%\Google Chrome.lnk"
-
-
-Call :Del "%Desktop%\qFlipper.lnk"
-Call :Del "%Desktop%\signal.lnk"
-
+Call :Del "Brave.lnk"
+Call :Del "Cisco Webex Meetings.lnk"
+Call :Del "Dell Display Manager*.lnk"
+Call :Del "Discord.lnk"
+Call :Del "Google Chrome.lnk"
+Call :Del "Firefox.lnk"
+Call :Del "LibreOffice *.lnk"
+Call :Del "Nextcloud.lnk"
+Call :Del "Microsoft Edge.lnk"
+Call :Del "Mumble.lnk"
+Call :Del "Oracle VM VirtualBox.lnk"
+Call :Del "pCloud Drive.lnk"
+Call :Del "qFlipper.lnk"
+Call :Del "RingCentral.lnk"
+Call :Del "Slack.lnk"
+Call :Del "Signal.lnk"
+Call :Del "Spotify.lnk"
+Call :Del "Visual Studio Code.lnk"
+Call :Del "Zoom.lnk"
+Call :Del "Zoom Workplace.lnk"
+Call :Del "Vonage Business.lnk"
 
 GoTo :EOF
+
+
+
 :Start
 REM Grab the path for the desktop from the registry
 for /f "tokens=2*" %%a in ('REG QUERY "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "Desktop"') do set "Desktop=%%~b"
@@ -35,9 +34,15 @@ Set Desktop=%UserProfile%\Desktop
 If Exist "%OneDrive%\Desktop" Set Desktop=%OneDrive%\Desktop
 GoTo :EOF
 
+
+
 :Del
 Set FileToDelete=%1
-If Not Exist %FileToDelete% GoTo :EOF
+Set Exists=0
+If Exist "%Desktop%\%FileToDelete%" Set Exists=1
+If Exist "C:\Users\Public\Desktop\%FileToDelete%" Set Exists=2
+If /i "%Exists%" EQU "0" GoTo :EOF
 Echo Deleting %FileToDelete%
-Del /q /f %FileToDelete%
+If /i "%Exists%" EQU "1" Set FileToDelete="%Desktop%\%FileToDelete%"
+If /i "%Exists%" EQU "2" Set FileToDelete="C:\Users\Public\Desktop\%FileToDelete%"
 GoTo :EOF
