e3d9b6f8e21525622f68855b39f6eae7e9f891dc
Coz <cozhelp@gmail.com>
2025-03-27 21:07:20 -0500

    Fix exit command in PatchMyPC script to use exit /b for proper termination

--- windows/maintenance/PatchMyPC.cmd
+++ windows/maintenance/PatchMyPC.cmd
@@ -3,4 +3,4 @@ If Exist "%Temp%\PatchMyPC.exe" Del "%Temp%\PatchMyPC.exe"
 curl https://patchmypc.com/freeupdater/PatchMyPC.exe -o "%Temp%\PatchMyPC.exe"
 start "a" /wait "%Temp%\PatchMyPC.exe" /s
 Del "%Temp%\PatchMyPC.exe"
-exit 0
+exit /b 0
