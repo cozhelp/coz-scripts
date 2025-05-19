d21e0ea5d46b5ff7e285502c2e49f90dc737d2ed
Coz <cozhelp@gmail.com>
2025-05-19 09:22:10 -0500

    Add script for deploying AD Connect on a domain controller

---
+++ windows/build_and_deploy/domain_controller_deploy_with_ADConnect.bat
@@ -0,0 +1,15 @@
+REM Use AHK to deploy AD Connect on a domain controller
+REM Set DNS ip to 127.0.0.1
+REM Use domain name "contoso.lan"
+REM Allow for vars to be passed to this script for: Domain Admin password, O365 password, O365 tenant name, and O365 domain name
+REM M365 domains to DC trusted domain name
+REM Disable IPv6 on the DC
+REM Set TLS 1.2
+
+REM ### Win 11 Build and Deploy Notes ###
+REM Build win 11 machine and auto domain join
+REM Disable IPV6
+
+
+
+
